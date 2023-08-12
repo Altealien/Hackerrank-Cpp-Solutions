cdc0f0190d57fc8b0d9cc3917d31283eb2716fa8
Altealien <omotayodavid657@gmail.com>
2023-08-12 22:59:16 +0100

    Initial Commit

---
+++ Problem Solving/Algorithms/NumberLineJumps.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+std::string kangaroo(int x1, int v1, int x2, int v2) {
+    double n = static_cast<double>((x2-x1))/(v1-v2); // this calculates the number of jumps
+    /*IF N IS A +VE INTEGER, IT MEANS THE KANGAROOS WILL EVENTUALLY MEET*/
+    if(n >=0 && n == static_cast<int>(n)){
+        return "YES";
+    }
+    else{
+        return "NO";
+    }
+}
+
+int main(){
+    std::cout << kangaroo(21,6,47,3) << std::endl;
+}
