21e61d27972cbc76fd1da592a4938c677929ebcf
Altealien <omotayodavid657@gmail.com>
2023-08-17 00:43:26 +0100

    Initial Commit

--- Problem Solving/Algorithms/NumberLineJumps.cpp
+++ Problem Solving/Algorithms/NumberLineJumps.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 std::string kangaroo(int x1, int v1, int x2, int v2) {
     double n = static_cast<double>((x2-x1))/(v1-v2); // this calculates the number of jumps
