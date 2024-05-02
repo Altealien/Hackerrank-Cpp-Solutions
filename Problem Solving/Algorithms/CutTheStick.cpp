90b2b29bb2a4484d9537b2e62f961c8a6b61488b
Altealien <omotayodavid657@gmail.com>
2024-05-02 03:33:14 +0100

    Initial Commit

--- Problem Solving/Algorithms/CutTheStick.cpp
+++ Problem Solving/Algorithms/CutTheStick.cpp
@@ -6,8 +6,8 @@ std::vector<int> cutTheSticks(std::vector<int> arr) {
     std::vector<int> size;
     while(arr.size() != 0){
         size.push_back(arr.size());
-        auto minLength = *std::min_element(arr.begin(), arr.end());
-        arr.erase(std::remove(arr.begin(), arr.end(), minLength), arr.end());
+        auto minLength = *std::min_element(arr.begin(), arr.end()); // Finds minimum length in vector
+        arr.erase(std::remove(arr.begin(), arr.end(), minLength), arr.end()); // Removes all occurrences of minimum length
         for(int i = 0; i<arr.size(); i++){
             if(arr[i] > minLength){
                 arr[i] -= minLength;
